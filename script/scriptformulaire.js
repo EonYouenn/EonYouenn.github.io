39236d0526d9d7a79f07fa438fc5f2d421ae7b35
EonYouenn <youenn.eon@gmail.com>
2022-12-01 00:14:29 +0100

    message

--- script/scriptformulaire.js
+++ script/scriptformulaire.js
@@ -5,3 +5,12 @@ function formulaire() {
     let mail=document.getElementById('mail').value;
     alert("Nom : "+ nom +" ,prenom : "+ prenom +" ,numéro étudiant : e"+ numetu +" ,adresse mail : "+ mail);
 }
+
+function prix() {
+    let prix=3;
+    let annales=document.getElementById('annales').checked;
+    let event=document.getElementById('event').checked;
+    if(annales){prix+=3};
+    if(event){prix+=20};
+    document.getElementById('prix').value=prix;
+}
